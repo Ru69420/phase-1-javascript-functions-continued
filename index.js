bafd0be39dcbf59778671470c3f47113038b38f2
RuthTadesse <tib6297@gmail.com>
2022-01-13 16:39:38 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+saturdayFun ();
+function saturdayFun(){
+    System.out.println("This Saturday, I want to ${roller-skate}, !")
+
+}
+
+function mondayWork (){
+   console.log ("go to the office")
+}
+
+function wrapAdjective (){
+    
+}
