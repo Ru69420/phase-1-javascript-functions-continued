0cd03053f2d4b6a0861201cdd624b0da269cc1a1
RuthTadesse <tib6297@gmail.com>
2022-02-21 15:38:57 -0500

    commit tree 986f97475c312f73b01f94c0b2b2e9af2751b4c6

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-// code your solution here
-saturdayFun ();
-function saturdayFun(){
-    System.out.println("This Saturday, I want to ${roller-skate}, !")
+
+
+function saturdayFun(activity1){
+    console.log(`This Saturday, I want to ${activity1}!`)
+
+}
+saturdayFun("roller-skate")
+saturdayFun("bathe my dog")
+
+function mondayWork(activity2){
+    console.log(`This Monday, I will ${activity2}.`)
 
 }
 
-function mondayWork (){
-   console.log ("go to the office")
-}
+mondayWork("go to the office")
+mondayWork("work from home")
 
-function wrapAdjective (){
-    
+function wrapAdjective(){
+    let result = wrapAdjective('*')
+      let emphatic = result("a hard worker")
 }
